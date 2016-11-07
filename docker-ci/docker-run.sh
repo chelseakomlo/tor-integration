5a61a1df564720d90814949acd2248489d83476a
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2016-11-06 22:25:46 -0500

    add fixes for docker ci

--- docker-ci/docker-run.sh
+++ docker-ci/docker-run.sh
@@ -1,4 +1,4 @@
 
-docker run -p 4747:4747 -p 9009:9009 -p 9010:9010 -i -t tor-integration /bin/bash -c "cd tor; sh autogen.sh &&
+docker run -p 4747:4747 -p 9009:9009 -p 9010:9010 -u integ-tester -it tor-integration /bin/bash -c "cd tor; sh autogen.sh &&
   ./configure --disable-asciidoc && make && make install; make test-network;
   make test-stem"
